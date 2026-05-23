bcf63e632ea91177fe2741ec15463a0cfa629ba4
MinhChien96 <MinhChien96@users.noreply.github.com>
2026-05-23 09:05:24 +0000

    deploy: 49769b3ec93b9e34fb88872897fa15e331a12e93

---
+++ mf-entry-bootstrap-0.js
@@ -0,0 +1,17 @@
+
+const __mfCacheGlobalKey = "__mf_module_cache__";
+globalThis[__mfCacheGlobalKey] ||= { share: {}, remote: {} };
+globalThis[__mfCacheGlobalKey].share ||= {};
+globalThis[__mfCacheGlobalKey].remote ||= {};
+const __mfModuleCache = globalThis[__mfCacheGlobalKey];
+
+const __mfImport = (src) =>
+  globalThis.System && typeof globalThis.System.import === 'function'
+    ? globalThis.System.import(src)
+    : import(src);
+(async () => {
+  const { initHost } = await __mfImport("https://minhchien96.github.io/micro-frontend/assets/hostInit-DY5j32Ef.js");
+  const runtime = await initHost();
+  const __mfRemotePreloads = [runtime.loadRemote("mfe_accounts/AccountsApp"),runtime.loadRemote("mfe_auth/Login"),runtime.loadRemote("mfe_cards/CardsApp"),runtime.loadRemote("mfe_loans/LoansApp"),runtime.loadRemote("mfe_profile/ProfileApp"),runtime.loadRemote("mfe_transfer/TransferApp")];
+  await Promise.all(__mfRemotePreloads);
+})().then(() => __mfImport("https://minhchien96.github.io/micro-frontend/assets/index-DbUlfOmF.js"));
