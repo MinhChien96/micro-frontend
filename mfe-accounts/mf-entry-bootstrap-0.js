cee81563eb4cd6c578e7f6424a1f060fcaace0a9
MinhChien96 <MinhChien96@users.noreply.github.com>
2026-05-23 05:01:49 +0000

    deploy: b92cb4f116c928411e6423807320a63a83343d16

---
+++ mfe-accounts/mf-entry-bootstrap-0.js
@@ -0,0 +1,17 @@
+
+const __mfCacheGlobalKey = "__mf_module_cache__";
+globalThis[__mfCacheGlobalKey] ||= { share: {}, remote: {} };
+globalThis[__mfCacheGlobalKey].share ||= {};
+globalThis[__mfCacheGlobalKey].remote ||= {};
+const __mfModuleCache = globalThis[__mfCacheGlobalKey];
+
+const __mfImport = (src) =>
+  globalThis.System && typeof globalThis.System.import === 'function'
+    ? globalThis.System.import(src)
+    : import(src);
+(async () => {
+  const { initHost } = await __mfImport("https://minhchien96.github.io/micro-frontend/mfe-accounts/assets/hostInit-DigbOFoT.js");
+  const runtime = await initHost();
+  const __mfRemotePreloads = [];
+  await Promise.all(__mfRemotePreloads);
+})().then(() => __mfImport("https://minhchien96.github.io/micro-frontend/mfe-accounts/assets/index-B0ticXBO.js"));
