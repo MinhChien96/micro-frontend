9f3b2f28ec9ac10d857b63974addf85f07c483ab
MinhChien96 <MinhChien96@users.noreply.github.com>
2026-05-23 08:40:21 +0000

    deploy: 1d640472bfd3c72442ca4f1c530e2707a5f7a1cc

---
+++ mfe-loans/mf-entry-bootstrap-0.js
@@ -0,0 +1,17 @@
+
+const __mfCacheGlobalKey = "__mf_module_cache__";
+globalThis[__mfCacheGlobalKey] ||= { share: {}, remote: {} };
+globalThis[__mfCacheGlobalKey].share ||= {};
+globalThis[__mfCacheGlobalKey].remote ||= {};
+const __mfModuleCache = globalThis[__mfCacheGlobalKey];
+
+const __mfImport = (src) =>
+  globalThis.System && typeof globalThis.System.import === 'function'
+    ? globalThis.System.import(src)
+    : import(src);
+(async () => {
+  const { initHost } = await __mfImport("https://minhchien96.github.io/micro-frontend/mfe-loans/assets/hostInit-dEBs7yjw.js");
+  const runtime = await initHost();
+  const __mfRemotePreloads = [];
+  await Promise.all(__mfRemotePreloads);
+})().then(() => __mfImport("https://minhchien96.github.io/micro-frontend/mfe-loans/assets/index-CT556Lpk.js"));
